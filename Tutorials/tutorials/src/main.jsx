457a07ad6482424636a6c71e8cacc7b9c9a49bc5
adhish1478 <adhish.aravind7@gmail.com>
2025-09-01 16:57:11 +0530

    poster-card

--- Tutorials/tutorials/src/main.jsx
+++ Tutorials/tutorials/src/main.jsx
@@ -1,20 +1,7 @@
 import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
-import Header from "./components/Header"
-import MainContent from "./components/MainComponent"
-import Footer from "./components/Footer"
-
-
-function Page() {
-    return (
-        <>
-            <Header />
-            <MainContent />
-            <Footer />
-        </>
-    )
-}
+import App from "./App"
 
 root.render(
-    <Page />
+    <App />
 )
