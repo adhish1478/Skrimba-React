a6b488e9da02b1ffdffb1763355efb1cd063eb89
adhish1478 <adhish.aravind7@gmail.com>
2025-09-01 15:34:30 +0530

    separate component files

--- Tutorials/tutorials/src/main.jsx
+++ Tutorials/tutorials/src/main.jsx
@@ -1,40 +1,9 @@
 import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
+import Header from "./components/Header"
+import MainContent from "./components/MainComponent"
+import Footer from "./components/Footer"
 
-function Header() {
-    return (
-        <header className="header">
-            <img src="src/react-logo.png" className="nav-logo" alt="React logo" />
-            <nav>
-                <ul className="nav-list">
-                    <li className="nav-list-item">Pricing</li>
-                    <li className="nav-list-item">About</li>
-                    <li className="nav-list-item">Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-function MainContent() {
-    return (
-        <main>
-            <h1>Reason I am excited to learn React</h1>
-            <ol>
-                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-                <li>I am more likely to get a job as a front end developer if I know React</li>
-            </ol>
-        </main>
-    )
-}
-
-function Footer() {
-    return (
-        <footer className= "footer">
-            <small>© 2024 Ziroll development. All rights reserved.</small>
-        </footer>
-    )
-}
 
 function Page() {
     return (
