879ea2ba21c9076042b20401cedb65228f48a08e
adhish1478 <adhish.aravind7@gmail.com>
2025-09-01 17:23:40 +0530

    first section over

--- Tutorials/tutorials/src/components/NavBar.jsx
+++ Tutorials/tutorials/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src="/src/react-logo.png" alt="React logo" />
+                <img src="/src/images/react-logo.png" alt="React logo" />
                 <span>ReactFacts</span>
             </nav>
         </header>
