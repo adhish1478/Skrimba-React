157d55512517faf7b7b9f0d9fa4f9e1913d41803
adhish1478 <adhish.aravind7@gmail.com>
2025-09-10 14:39:10 +0530

    Header Section done

---
+++ chef-claude/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="src/images/chef-claude-icon.png" alt="Chef Claude Logo" className="header__logo" />
+            <span>Chef-Claude</span>
+        </header>
+        )
+}
