f54afaafa3a7c627da0a9b203bdb350e5c937743
adhish1478 <adhish.aravind7@gmail.com>
2025-09-11 14:00:42 +0530

    state and action done

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
+
 export default function Main() {
+
+    const [ingredients, setIngredients] = useState([]);
+
+    function handleSubmit(formData) {
+            const ingredient= formData.get("ingredient");
+            setIngredients(prevIngredients => [...prevIngredients, ingredient]);
+            console.log(ingredients);
+
+        }
+
+    const ingredientListElements= ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
     return (
         <main>
-            <form className="main_form">
+            <form className="main_form" action={handleSubmit}>
                 <input 
                 type="text"
                 placeholder="e.g. Potato"
                 aria-label="Add-ingredient"
+                name= "ingredient"
                 />
                 <button type="submit">Add Ingredient</button>
             </form>
+            <ul>
+                {ingredientListElements}
+            </ul>
         </main>
     )
 }
