c66a161277866dec304b3908785918c309148a7f
adhish1478 <adhish.aravind7@gmail.com>
2025-09-15 19:13:09 +0530

    boilerplate added

--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -13,23 +13,7 @@ export default function Main() {
             .then(data => console.log(data.data.memes))
     }, [])
     
-    /**
-     * Challenge:
-     * Get an array of memes from the imgflip API as soon as
-     * this component renders for the first time.
-     * Check the imgflip documentation for the correct URL.
-     * Save the array of memes (not the whole response
-     * data) to state. (For this app, we'll randomly choose
-     * one of the memes from this array when the user clicks
-     * the "Get a new meme image" button, but we'll do that in
-     * a separate challenge.)
-     * 
-     * Hint: for now, don't try to use an async/await function.
-     * Instead, use `.then()` to resolve the promises
-     * from using `fetch`. We'll learn why after this challenge.
-     */
-    
-    
+
     
     function handleChange(event) {
         const {value, name} = event.currentTarget
