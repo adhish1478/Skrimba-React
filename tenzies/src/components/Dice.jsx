a01ac714d0b56d09787a73145810eb79d4e1aa2d
adhish1478 <adhish.aravind7@gmail.com>
2025-09-16 13:31:20 +0530

    styling

--- tenzies/src/components/Dice.jsx
+++ tenzies/src/components/Dice.jsx
@@ -1,7 +1,6 @@
 export default function Dice(props) {
     return (
-        <div className="individual-die">
-            <h2>{props.value}</h2>
-        </div>
+            <button className="die">{props.value}</button>
+        
     )
 }
